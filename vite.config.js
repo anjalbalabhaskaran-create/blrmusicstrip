e360a3d95022890806953b63cbbb8a47cfe8a062
Anjal B <anjalb@Anjals-MacBook-Pro.local>
2026-07-27 16:53:13 +0530

    Fix: Add base path /blrmusicstrip/ to vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/blrmusicstrip/',
   plugins: [react({
     jsxRuntime: 'automatic' // This allows JSX without importing React
   })]
